8289e954d56bcdc0ea90ce4063340a9706922e44
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2023-02-21 16:11:51 +0530

    final commit

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero(props: iProps) {
     <section id="home">
       <div className="min-h-screen relative flex lg:flex-row flex-col-reverse lg:items-end justify-center items-center">
         <div className="absolute h-full  lg:w-4/12 w-6/12 top-0 right-0 bg-lightBgColor bottom-0 -z-10">
-          <h1 className="rotate-90 absolute top-[40%] md:right-[-24%] right-[-138px] sm:right-[-110px] text-[#EAF2FA] select-none">
+          <h1 className="rotate-90 absolute top-[40%] md:right-[-30%] xl:right-[-40%] right-[-128px] sm:right-[-110px] text-[#EAF2FA] select-none">
             {home.bannerText}{" "}
             <span className="text-dark_primary">{home.bannerTextSpan}</span>
           </h1>
@@ -35,7 +35,7 @@ export default function Hero(props: iProps) {
 
         <div className="lg:h-[500px] mt-10 lg:mt-0 w-[80%] lg:w-[32%] text-center lg:text-right select-none">
           <p className="text-xl text-slate-600"> Hey there, It's </p>
-          <h2 className="text-4xl lg:text-5xl xl:text-7xl sm:text-5xl md:text-6xl">
+          <h2 className="text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl sm:text-5xl md:text-6xl">
             {" "}
             {home.fullName}{" "}
           </h2>
@@ -65,7 +65,7 @@ export default function Hero(props: iProps) {
           />
           <img
             src={home.heroImgForMob}
-            className="h-full object-cover max-w-sm lg:hidden"
+            className="h-full object-cover max-w-[244px] sm:max-w-sm lg:hidden"
           />
         </div>
       </div>
